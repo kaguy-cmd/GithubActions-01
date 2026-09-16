9ccc1ad25bfba4c27066533c377cfd54b80ebfb5
kaguy-cmd <mvkrishna440@gmail.com>
2026-09-16 17:46:43 +0530

    my first pipeline

---
+++ src/myapp.java
@@ -0,0 +1,15 @@
+public class myapp {
+
+    void method01() {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+
+        myapp obj = new myapp();
+        obj.method01();
+    }
+}
